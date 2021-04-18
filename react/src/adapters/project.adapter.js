b21386c89be1ec52f8b6c1f68c51bd4d4f164eb8
VoN <sudo.von.contact@gmail.com>
2021-04-17 23:32:20 -0500

    feat: :sparkles: Add the rest of adapters

---
+++ react/src/adapters/project.adapter.js
@@ -0,0 +1,16 @@
+/* Http request handler. */
+import httpClient from '.'
+/* Custom interceptors. */
+import { errorInterceptor } from '../interceptors/error.interceptor'
+
+const ENDPOINT = '/projects'
+
+httpClient.interceptors.response.use(errorInterceptor)
+
+/* Get the projects that will be printed in the Portfolio. */
+const getProjects = () =>
+    new Promise( ( resolve, reject ) => 
+        httpClient.get(ENDPOINT)
+            .then(res => resolve(res.data))
+            .catch(err => reject(err))
+    )
