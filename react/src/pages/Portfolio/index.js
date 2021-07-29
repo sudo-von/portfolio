f2d28bdc1b0f91d831ffbf89cbb01fd18da6209b
VoN <sudo.von.contact@gmail.com>
2021-07-28 21:28:13 -0500

    feat: :sparkles: Add Navbar to the Portfolio component

--- react/src/pages/Portfolio/index.js
+++ react/src/pages/Portfolio/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 /* Material-ui. */
 import Container from '@material-ui/core/Container'
 /* Custom components. */
+import Navbar from 'components/Navbar/'
 import Profile from './Components/Profile/'
 
 const Portfolio = () => 
   <Container>
+    <Navbar/>
     <Profile/>
   </Container>
 
