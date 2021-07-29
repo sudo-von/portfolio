1df66169ab3b9f03f983f928ed9241b08bc42958
VoN <sudo.von.contact@gmail.com>
2021-07-28 21:26:54 -0500

    fix: :bug: Add 'px' to the border-radius style in the Image styled component

--- react/src/components/Image/Image.js
+++ react/src/components/Image/Image.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const Image = styled.img`
     width: 100%;
-    border-radius: ${ props => props.borderRadius ? props.borderRadius : '0'};
+    border-radius: ${ props => props.borderRadius ? props.borderRadius : '0'}px;
 `
 
 export default Image
