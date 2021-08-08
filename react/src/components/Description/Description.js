099ec02d2719e78709a0b2b64f21f44c64e8c5a6
VoN <sudo.von.contact@gmail.com>
2021-08-08 04:30:21 -0500

    feat: :sparkles: Add Description component

---
+++ react/src/components/Description/Description.js
@@ -0,0 +1,27 @@
+import { Fragment } from 'react'
+import PropTypes from 'prop-types'
+/* Custom components. */
+import H2 from 'components/H2/'
+import Bold from 'components/Bold/'
+
+const Description = ({ title, bold, children}) =>
+    <Fragment>
+        <H2>{ bold ? <Bold>{title}</Bold> : title }</H2>
+        <div>
+
+        {children}
+        </div>
+    </Fragment>
+
+Description.propTypes = {
+    title: PropTypes.string,
+    bold: PropTypes.bool,
+    children: PropTypes.node.isRequired
+}
+
+Description.defaultProps = {
+    title: '',
+    bold: false
+}
+
+export default Description
