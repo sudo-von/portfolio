efcb5234ded6db6b15891e37e9c16ffb80f1851b
VoN <sudo.von.contact@gmail.com>
2021-08-08 03:27:47 -0500

    feat: :sparkles: Update Profile component, instead of use harcoded elements, it will use custom components

--- react/src/pages/Portfolio/Components/Profile/Profile.js
+++ react/src/pages/Portfolio/Components/Profile/Profile.js
@@ -1,24 +1,22 @@
 /* Material ui. */
 import Grid from '@material-ui/core/Grid'
-import Hidden from '@material-ui/core/Hidden'
 /* Custom components. */
 import ProfileName from './Components/ProfileName'
-import H5 from 'components/H5'
+import ProfileDescription from './Components/ProfileDescription'
+import ProfilePhrase from './Components/ProfilePhrase'
+import ProfileBackground from './Components/ProfileBackground'
 import Bold from 'components/Bold/'
-import Image from 'components/Image/'
 
 const Profile = () =>
   <Grid container direction="row" alignItems="center" style={styles.grid}>
     <Grid item xs={12} md={6}>
       <ProfileName name='Jesús Rodríguez'/>
-      <H5><Bold>Desarrollador web</Bold> y de <Bold>aplicaciones móviles</Bold></H5>
-      <H5>Apasionado del <Bold>hacking ético</Bold></H5>
-      <small className='accent'>“La única forma de hacer un gran trabajo, es amar lo que haces”.</small>
+      <ProfileDescription><Bold>Desarrollador web</Bold> y de <Bold>aplicaciones móviles</Bold></ProfileDescription>
+      <ProfileDescription>Apasionado del <Bold>hacking ético</Bold></ProfileDescription>
+      <ProfilePhrase phrase='“La única forma de hacer un gran trabajo, es amar lo que haces”.'/>
     </Grid>
     <Grid item md={6}>
-      <Hidden only={['xs', 'sm']}>
-        <Image borderRadius={5} src='https://images8.alphacoders.com/101/thumb-1920-1014794.jpg'/>
-      </Hidden>
+      <ProfileBackground src='https://images8.alphacoders.com/101/thumb-1920-1014794.jpg'/>
     </Grid>
   </Grid>
 
