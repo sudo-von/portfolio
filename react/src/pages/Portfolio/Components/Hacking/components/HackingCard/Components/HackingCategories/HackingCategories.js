73bf760ecc94de45629b1b1c56f8845a4afc6317
VoN <sudo.von.contact@gmail.com>
2021-08-08 19:32:12 -0500

    feat: :sparkles: Add HackingCategories component

---
+++ react/src/pages/Portfolio/Components/Hacking/components/HackingCard/Components/HackingCategories/HackingCategories.js
@@ -0,0 +1,39 @@
+import { Fragment } from 'react'
+import PropTypes from 'prop-types'
+/* Material-ui.*/
+import Chip from '@material-ui/core/Chip'
+import Typography from '@material-ui/core/Typography'
+
+const HackingCategories = ({ id, categories }) =>
+    <Fragment>
+        <Typography variant='caption' display='block' color='textSecondary'>Tecnologías</Typography>
+        { categories && categories.map(category => 
+            <Chip 
+                key={`${id}-${category}`} 
+                size='small' 
+                variant='outlined'
+                style={styles.chip} 
+                label={category}
+            />
+        )}
+    </Fragment>
+
+const styles = {
+  chip: {
+    marginTop: 15,
+    marginRight: 2
+  }
+}
+
+HackingCategories.propTypes = {
+    id: PropTypes.string,
+    tech_stack: PropTypes.array
+}
+
+HackingCategories.defaultProps = {
+    id: '',
+    tech_stack: []
+}
+
+
+export default HackingCategories
