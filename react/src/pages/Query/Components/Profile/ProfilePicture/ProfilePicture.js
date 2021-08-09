7b5509a2fbd32cbf2775afeca57a9fb21a885c84
VoN <sudo.von.contact@gmail.com>
2021-08-09 02:36:58 -0500

    feat: :sparkles: Add ProfilePicture component

--- react/src/pages/Query/Components/Profile/ProfilePicture/ProfilePicture.js
+++ react/src/pages/Query/Components/Profile/ProfilePicture/ProfilePicture.js
@@ -1,11 +1,27 @@
+import PropTypes from 'prop-types'
 /* Custom components. */
 import Image from 'components/Image'
+/* Material-ui components. */
+import Box from '@material-ui/core/Box'
 
-const ProfilePicture = ({ src='https://giffiles.alphacoders.com/191/191571.gif'}) =>
-    <Image 
-        borderRadius={20} 
-        src={src}
-    />
+const ProfilePicture = ({ src }) =>
+    <Box display='flex' justifyContent='center' alignItems='center' style={styles.box}>
+        <Image
+            height={200}
+            width={200}
+            borderRadius={100} 
+            src={src}
+        />
+    </Box>
 
+const styles = {
+    box : {
+        margin : 15
+    }
+}
+
+ProfilePicture.propTypes = {
+    src: PropTypes.string
+}
 
 export default ProfilePicture
