67f234e61280074bea8cba170ffb13730cf4ce81
VoN <sudo.von.contact@gmail.com>
2021-08-08 23:38:36 -0500

    feat: :sparkles: Handle error and handle when there are no elements in the Paginator component

--- react/src/components/Paginator/Paginator.js
+++ react/src/components/Paginator/Paginator.js
@@ -1,21 +1,28 @@
 /* Material-ui. */
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
+import Alert from '@material-ui/lab/Alert'
 import Pagination from '@material-ui/lab/Pagination'
 /* High order components. */
 import withLoading from 'hocs/withLoading'
+import withError from 'hocs/withError'
 
-// TODO: Handle when there are no elements.
-// TODO: Fix pagination count.
-const Paginator = ({ data : { total }, page, limit, handlePage, children}) =>
-    <Grid container spacing={2}>
-        {children}
-        <Grid item xs={12}>
-            <Box display='flex' justifyContent='center' alignItems='center'>
-                <Pagination count={Math.round(total/limit)} page={page} onChange={handlePage} variant='outlined'/>
-            </Box>
+const Paginator = ({ data : { total }, page, limit, handlePage, children}) => {
+
+    if(total == 0){
+        return <Alert severity="info">No hay datos disponibles</Alert>
+    }
+
+    return(
+        <Grid container spacing={2}>
+            {children}
+            <Grid item xs={12}>
+                <Box display='flex' justifyContent='center' alignItems='center'>
+                    <Pagination count={Math.round(total/limit)} page={page} onChange={handlePage} variant='outlined'/>
+                </Box>
+            </Grid>
         </Grid>
-    </Grid>
+    )
+}
 
-
-export default withLoading(Paginator, 'Cargando...')
+export default withError(withLoading(Paginator, 'Cargando...'),' Ha ocurrido al cargar este apartado, intenta de nuevo más tarde')
